795ac768d655047876c697882e8fc80e6c7a18ba
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-07-07 23:00:16 -0700

    worked with james exchanging code and ideas

--- app.js
+++ app.js
@@ -1,47 +1,116 @@
 'use strict';
-  var images = [];//place holder for objects
-  var randNum =[];
-
-function Image(imageName, path){
-  this.imageName = imageName; // name of each image
-  this.path = path; //path where the file lives
+var bag = new Items('bag', 'img/bag.jpg');
+var banana = new Items('banana', 'img/banana.jpg');
+var bathroom = new Items('bathroom', 'img/bathroom.jpg');
+var boots = new Items('boots', 'img/boots.jpg');
+var breakfest = new Items('breakfast', 'img/breakfast.jpg');
+var bubblegum = new Items('bubblegum', 'img/bubblegum.jpg');
+var chair = new Items('chair', 'img/chair.jpg');
+var cthulhu = new Items('cthulhu', 'img/cthulhu.jpg');
+var dog = new Items('dogDuck', 'img/dog-duck.jpg');
+var dragon = new Items('dragon', 'img/dragon.jpg');
+var pen = new Items('pen', 'img/pen.jpg');
+var petSweep = new Items('petSweep', 'img/pet-sweep.jpg');
+var scissors = new Items('scissors', 'img/scissors.jpg');
+var shark = new Items('shark', 'img/shark.jpg');
+var sweep = new Items('sweep', 'img/sweep.png');
+var tauntaun = new Items('tauntaun', 'img/tauntaun.jpg');
+var unicorn = new Items('unicorn', 'img/unicorn.jpg');
+var usb = new Items('usb', 'img/usb.gif');
+var waterCan = new Items('waterCan', 'img/water-can.jpg');
+var wineGlass = new Items('wineGlass', 'img/wine-glass.jpg');
+var itemsArr = [bag, banana, bathroom, boots, breakfest, chair, cthulhu, dog, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+function Items(name, img){
+  this.name = name;
+  this.img = img;
   this.timesDisplayed = 0; //starts at 0 because nothing was selected
   this.timesClicked = 0;
-  images.push(this);
-};
-//listing out the products creating new imgs
-  var bag = new Image('img/bag.jpg');
-  var banana= new Image('img/banana.jpg');
-  var bathroom= new Image('img/bathroom.jpg');
-  var breakfast= new Image('img/breakfast.jpg');
-  var bubblegum= new Image('img/bubblegum.jpg');
-  var chair= new Image('img/cthulhu.jpg');
-  var dogDuck= new Image('img/dog-duck.jpg');
-  var dragon= new Image('img/dragon.jpg');
-  var pen= new Image('img/pen.jpg');
-  var petSweep= new Image('img/pet-sweep.jpg');
-  var scissors= new Image('img/scissors.jpg');
-  var shark= new Image('img/shark.jpg');
-  var sweep= new Image('img/sweep.png');
-  var tauntuan= new Image('img/tauntuan.jpg');
-  var unicorn= new Image('img/unicorn.jpg');
-  var usb= new Image('img/usb.gif');
-  var waterCan= new Image('img/water-can.jpg');
-  var wineGlass= new Image('img/wine-glass.jpg');
+}
+var photo1 = document.getElementById('img1');
+var photo2 = document.getElementById('img2');
+var photo3 = document.getElementById('img3');
+var photoArr = [photo1, photo2, photo3];
+for (var i = 0; i < photoArr.length; i++){
+  photoArr[i].src = itemsArr[randomNum()].img;
+}
+function randomNum() {
+  var min = 1;
+  var max = 20;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
-console.log(images);
-console.log(images.length);
 
-//need to create a function that randomly creates a number
 
-function randNum() {
-  var selectOne = Math.floor((Math.random()* images.length) + 1);
-console.log(selectOne);//can not see the log
-  var selectTwo = Math.floor((Math.random()* images.length) + 1);
-  console.log(selectTwo);
-  var selectThree = Math.floor((Math.random() * images.length) + 1);
-  console.log(selectThree);
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   var images = [];//place holder for objects
+//   var randNum =[];
+//
+// function Image(imageName, path){
+//   this.imageName = imageName; // name of each image
+//   this.path = path; //path where the file lives
+//   this.timesDisplayed = 0; //starts at 0 because nothing was selected
+//   this.timesClicked = 0;
+//   images.push(this);
+// };
+// //listing out the products creating new imgs
+//   var bag = new Image('img/bag.jpg');
+//   var banana= new Image('img/banana.jpg');
+//   var bathroom= new Image('img/bathroom.jpg');
+//   var breakfast= new Image('img/breakfast.jpg');
+//   var bubblegum= new Image('img/bubblegum.jpg');
+//   var chair= new Image('img/cthulhu.jpg');
+//   var dogDuck= new Image('img/dog-duck.jpg');
+//   var dragon= new Image('img/dragon.jpg');
+//   var pen= new Image('img/pen.jpg');
+//   var petSweep= new Image('img/pet-sweep.jpg');
+//   var scissors= new Image('img/scissors.jpg');
+//   var shark= new Image('img/shark.jpg');
+//   var sweep= new Image('img/sweep.png');
+//   var tauntuan= new Image('img/tauntuan.jpg');
+//   var unicorn= new Image('img/unicorn.jpg');
+//   var usb= new Image('img/usb.gif');
+//   var waterCan= new Image('img/water-can.jpg');
+//   var wineGlass= new Image('img/wine-glass.jpg');
+//
+// console.log(images);
+// console.log(images.length);
+//
+// //need to create a function that randomly creates a number
+//
+// function randNum() {
+//   var selectOne = Math.floor((Math.random()* images.length) + 1);
+// console.log(selectOne);//can not see the log
+//   var selectTwo = Math.floor((Math.random()* images.length) + 1);
+//   console.log(selectTwo);
+//   var selectThree = Math.floor((Math.random() * images.length) + 1);
+//   console.log(selectThree);
+// };
 // now that i have consturcter function that brings back the array for images and consturcter for three different random numbers
 // need to create a new one that combies the two
 
